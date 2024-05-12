b510eb83208a4173f044d3cdf3e6e45602e18246
ignormies <bryce.beagle@gmail.com>
2024-05-12 10:26:57 -0700

    Use append instead of insert for sys.path

--- stage_0/main.py
+++ stage_0/main.py
@@ -5,7 +5,7 @@ from typing import Final
 CLONE_DIR: Final = Path("/tmp/pycon-italia-2024/")
 assert CLONE_DIR.exists()
 
-sys.path.insert(0, str(CLONE_DIR))
+sys.path.append(str(CLONE_DIR))
 
 from what import FooBar
 print(FooBar.hello_world)
